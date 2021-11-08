0bb0105e7178d7a27f4d1a13727e988f62b3976d
valerity88 <valerity@inbox.ru>
2021-11-08 14:10:36 +0700

    My

--- news/utils.py
+++ news/utils.py
@@ -1,11 +1,11 @@
-#class MyMixin(object):
-#    mixin_prop = ''
+class MyMixin(object):
+    mixin_prop = ''
 
-#    def get_prop(self):
-#        return self.mixin_prop.upper()
-#
-#    def get_upper(self, s):
-#        if isinstance(s, str):
-#            return s.upper()
-#        else:
-#            return s.title.upper()
+    def get_prop(self):
+        return self.mixin_prop.upper()
+
+    def get_upper(self, s):
+        if isinstance(s, str):
+            return s.upper()
+        else:
+            return s.title.upper()
